89fbfca3e0e792828445e87a5101f2bb3bfdfa21
Nebil-Abdulfetah <159676747+Nebil-Abdulfetah@users.noreply.github.com>
2025-02-23 22:38:06 +0300

    Added testimonials section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
 const menuOpenButton = document.getElementById("menu-open-button");
 const menuCloseButton = document.getElementById("menu-close-button");
 
-
 //gives or removes the show-mobile-menu to the body
 menuOpenButton.addEventListener("click", () => {
-    document.body.classList.toggle("show-mobile-menu")
-})
+  document.body.classList.toggle("show-mobile-menu");
+});
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+//Initialize Swiper
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
